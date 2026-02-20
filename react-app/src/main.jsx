3f4997a6293416de78eaecd4f8c428d6c9212e6e
Yash Jain <yashjain@Yashs-MacBook-Air-6.local>
2026-02-21 02:22:42 +0530

    Fix CSS loading order to restore content card UI

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './tailwind.css'
 import './styles/responsive.css';
 import './styles/pages-responsive.css';
 import './styles/watch-party-responsive.css';
 import './styles/admin-responsive.css';
 import './styles/content-card-responsive.css';
 import './styles/responsive-utilities.css';
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
